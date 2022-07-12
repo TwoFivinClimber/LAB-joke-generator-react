4b10eab4afae3984441a46aab5ffb38551866917
Brett Hughes <twofivinclimber@gmail.com>
2022-07-11 22:00:05 -0500

    no progress

--- api/jokeData.js
+++ api/jokeData.js
@@ -4,7 +4,6 @@ const endpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart
 
 const getJoke = async () => {
   const jokeCall = await axios.get(endpoint);
-
   return jokeCall.data;
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
+
 function Home() {
+  const [buttonText] = useState('Get a Joke');
+  const [jokeText] = useState('The Joke is On You');
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
       style={{
         height: '90vh',
         padding: '30px',
-        maxWidth: '400px',
+        maxWidth: '450px',
         margin: '0 auto',
       }}
     >
-      <h1>Welcome Home!</h1>
+      <h1>{jokeText}</h1>
+      <button type="button">{buttonText}</button>
     </div>
   );
 }
