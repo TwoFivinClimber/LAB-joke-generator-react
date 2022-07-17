dd3b43e9a7d44a3f191c926bdfcad50fa28d1364
Brett Hughes <twofivinclimber@gmail.com>
2022-07-17 16:14:20 -0500

    added a ping to check if server is up and running

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import React, { useEffect } from 'react';
-import Redirect from 'react-router-dom';
+import { useRouter } from 'next/router';
 import NavBar from '../components/NavBar';
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
   useEffect(() => {
     fetch('https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart')
       // eslint-disable-next-line consistent-return
       .then((response) => {
-        if (response.status !== 300) {
-          console.warn(response.status);
-          return <Redirect to="/maintenance" />;
+        if (response.status !== 200) {
+          router.push('./maintenance');
         }
       }).catch();
   });
